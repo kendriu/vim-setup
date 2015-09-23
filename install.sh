dd85a7342ab0710b2b9ba74b1116f2b9e51b9a0a
Andrzej Skupien <askupien@mirantis.com>
2015-09-23 15:48:01 +0200

    Preferring apt-get over yum

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 if  hash brew 2>/dev/null; then
 	brew install -y cmake the_silver_searcher vim
-elif hash yum 2>/dev/null; then
-	sudo yum install -y cmake the_silver_searcher vim
 elif hash apt-get 2>/dev/null; then
 	sudo apt-get install -y cmake silversearcher-ag vim
+elif hash yum 2>/dev/null; then
+	sudo yum install -y cmake the_silver_searcher vim
 fi
 
 vim -u ./.vimrc +PlugInstall +qall
