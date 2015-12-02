196a8832b789a6c3a1a1ee86be43823f8a79e933
Andrzej Skupień <kendriu@gmail.com>
2015-12-02 15:44:33 +0100

    Added installation of ctags

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 if  hash brew 2>/dev/null; then
 	brew install -y cmake the_silver_searcher vim
 elif hash apt-get 2>/dev/null; then
-	sudo apt-get install -y cmake silversearcher-ag vim build-essential python-dev
+	sudo apt-get install -y cmake ctags silversearcher-ag vim build-essential python-dev
 elif hash yum 2>/dev/null; then
 	sudo yum install -y cmake the_silver_searcher vim
 fi
